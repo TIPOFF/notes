54c5d18aa20f80b9c2ffc450a4e98a11343778ec
Drew Roberts <github@drewroberts.com>
2021-02-13 14:52:05 -0500

    Update to current version of service provider

--- src/NotesServiceProvider.php
+++ src/NotesServiceProvider.php
@@ -4,30 +4,20 @@ declare(strict_types=1);
 
 namespace Tipoff\Notes;
 
-use Illuminate\Support\Facades\Gate;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Tipoff\Notes\Models\Note;
 use Tipoff\Notes\Policies\NotePolicy;
+use Tipoff\Support\TipoffPackage;
+use Tipoff\Support\TipoffServiceProvider;
 
-class NotesServiceProvider extends PackageServiceProvider
+class NotesServiceProvider extends TipoffServiceProvider
 {
-    public function boot()
+    public function configureTipoffPackage(TipoffPackage $package): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-
-        parent::boot();
-    }
-
-    public function configurePackage(Package $package): void
-    {
-        $package->name('notes')
-            ->hasConfigFile()
-            ->hasTranslations();
-    }
-
-    public function registeringPackage()
-    {
-        Gate::policy(Note::class, NotePolicy::class);
+        $package
+            ->hasPolicies([
+                Note::class => NotePolicy::class,
+            ])
+            ->name('notes')
+            ->hasConfigFile();
     }
 }
