0e6d5c6150808908900eaaa72f3b8d6227372193
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-03-03 13:14:24 -0500

    add NoteResourceTest

---
+++ tests/Feature/Nova/NoteResourceTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Notes\Tests\Feature\Nova;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tipoff\TestSupport\Models\User;
+use Tipoff\Notes\Models\Note;
+use Tipoff\Notes\Tests\TestCase;
+
+class NoteResourceTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function index()
+    {
+        Note::factory()->count(4)->create();
+
+        $this->actingAs(self::createPermissionedUser('view notes', true));
+
+        $response = $this->getJson('nova-api/notes')
+            ->assertOk();
+
+        $this->assertCount(4, $response->json('resources'));
+    }
+}
